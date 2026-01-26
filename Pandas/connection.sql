d6ba91e255b09b27f63312991aa4e5cfbefb1185
Rohan Kondhalkar <rkondhalkars111@gmail.com>
2026-01-26 10:43:26 +0530

    Add files via upload

---
+++ Pandas/connection.sql
@@ -0,0 +1,17 @@
+use application;
+select * from customers;
+
+
+CREATE USER 'pandas_user'@'localhost' IDENTIFIED BY 'pandas123';
+
+GRANT ALL PRIVILEGES ON application.* TO 'pandas_user'@'localhost';
+
+FLUSH PRIVILEGES;
+
+GRANT ALL PRIVILEGES ON application.* 
+TO 'pandas_user'@'localhost';
+
+FLUSH PRIVILEGES;
+
+SHOW GRANTS FOR 'pandas_user'@'localhost';
+
